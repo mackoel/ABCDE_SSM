78e469efbda3796eb3d93eee8661b34a332b604d
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2020-03-16 11:14:48 +0300

    Delete Sampling.h

--- Sampling.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-double qaussian(double x);
-
-void metropolis_gasstings(const int num_iter, double *thetha);//theta structur
-
-double uniform_random(double mm, double nn);
-
-Thetha get_prev_iter_with_probabilities(Posterior *posterior);
