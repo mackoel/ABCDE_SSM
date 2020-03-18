8ee04641d9f1ae4ef761dbe701aa52fe23c259fd
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2020-03-18 17:23:18 +0300

    Update abcde.h

--- abcde.h
+++ abcde.h
@@ -21,7 +21,6 @@ public:
 		boost::property_tree::ptree pt;
 		boost::property_tree::ini_parser::read_ini(config_file, pt);
 		count_thread =  stoi(pt.get<std::string>("data.count_thread"));
-		deep_ini_file = pt.get<std::string>("data.name_deep_ini_file");
 		deep_exe = pt.get<std::string>("data.name_exe_file");
 		optimizing_model_exe = pt.get<std::string>("data.name_command_exe_file");
 		param_opt_model = pt.get<std::string>("data.param_command_exe_file");
@@ -39,7 +38,7 @@ public:
 	}
 	Distribution::Thetha crossover(int index)
 	{
-		double si_1 = generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, 0.5, 1), si_2 = generator.prior_distribution(NORM_WITH_PARAM, 0.5, 1), b = generator.prior_distribution(NORM_WITH_PARAM, 0.001, 0.001);
+		double si_1 = generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, 0.5, 1), si_2 = generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, 0.5, 1), b = generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, 0.001, 0.001);
 		Distribution::Thetha thetha_b, thetha_m, thetha_n, curr_thetha;
 	//	thetha_b = generator.get_prev_iter_with_probabilities(posterior);
 		thetha_m = posterior.thetha[rand() % (count_iter + 1)];
@@ -56,10 +55,10 @@ public:
 
 		double sigma_psi = generator.prior_distribution(Distribution::TYPE_DISTR::EXPON, 0.005);
 		double psi = generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, error, sigma_psi);
-		double alpha = ((generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, curr_thetha.n, SIGMA) * generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, curr_thetha.l, SIGMA) * generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, curr_thetha.lambda, SIGMA)) * psi) / ((generator.prior_distribution(NORM_WITH_PARAM, posterior.thetha[i].n, SIGMA) * generator.prior_distribution(NORM_WITH_PARAM, posterior.thetha[i].l, SIGMA) * generator.prior_distribution(NORM_WITH_PARAM, posterior.thetha[i].lambda, SIGMA)) * psi);
+		double alpha = ((generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, curr_thetha.n, SIGMA) * generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, curr_thetha.l, SIGMA) * generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, curr_thetha.lambda, SIGMA)) * psi) / ((generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, posterior.thetha[i].n, SIGMA) * generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, posterior.thetha[i].l, SIGMA) * generator.prior_distribution(Distribution::TYPE_DISTR::NORM_WITH_PARAM, posterior.thetha[i].lambda, SIGMA)) * psi);
+		return alpha;
 	}
 	string config_file;
-	string deep_ini_file;
 	string deep_exe;
 	string optimizing_model_exe;
 	string param_opt_model;
