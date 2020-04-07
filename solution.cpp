9f211a6cfe9fdf6e5b0f90a041aae116e9794b74
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2020-04-07 14:49:13 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -6,57 +6,62 @@ Solution::Solution(const Abcde& _main_model, const Deep& _aux_model) {
 	aux_model = _aux_model;
 	error = 0.0;
 	alpha = 0.0;
-	new_posterior.thetha = new Distribution::Thetha[main_model.count_iter];
-	new_posterior.w = new double[main_model.count_iter];
 }
-void Solution::copy_posterior(const Distribution::Posterior &posterior_to, const Distribution::Posterior& posterior_from)
+void Solution::copy_posterior(Distribution::Posterior &posterior_to,Distribution::Posterior& posterior_from)
 {
-	for (int i = 0; i < main_model.count_iter; i++)
-	{
-		posterior_to.thetha[i] = posterior_from.thetha[i];
-		posterior_to.w[i] = posterior_from.w[i];
-	}
+	Distribution::Thetha * tmp_thetha = posterior_to.thetha;
+	double* tmp_w = posterior_to.w;
+	posterior_to.thetha = posterior_from.thetha;
+	posterior_from.thetha = tmp_thetha;
+	posterior_to.w = posterior_from.w;
+	posterior_from.w = tmp_w;
 }
 void Solution::run()
 {
-	double error = 0.0, alpha;
 	for (int i = 0; i < main_model.count_iter; i++)
 	{
 		do {
 			main_model.curr_thetha = main_model.generator.generate_vector_param(Distribution::NORM_WITH_PARAM);
+			cout << "curr_thetha: ";
+			cout << main_model.curr_thetha.n << " ";
+			cout << main_model.curr_thetha.l << " ";
+			cout << main_model.curr_thetha.lambda << endl;
 			aux_model.act_with_config_file();
 			aux_model.prepare_tmp_deep_ini_file(main_model.curr_thetha, main_model.optimizing_model_exe, main_model.param_opt_model);
 			error = aux_model.run();
+			cout << "error = " << error << endl;
 		} while (error < main_model.config.eps);
 		main_model.posterior.thetha[i] = main_model.curr_thetha;
 		main_model.posterior.w[i] = 1.0 / main_model.count_iter;
+
 	}
+	print_log(-1);
 	//current_values in Soultion, result_value in Abcde
 	for (int t = 0; t < main_model.t; t++)
 	{
-		copy_posterior(new_posterior, main_model.posterior);
+		copy_posterior(main_model.new_posterior, main_model.posterior);
 		for (int i = 0; i < main_model.count_iter; i++)
 		{
 			if (rand() < 0.05)
 			{
-				curr_thetha = main_model.mutation(i);
+				main_model.curr_thetha = main_model.mutation(i);
 			}
 			else
 			{
-				curr_thetha = main_model.crossover(i);
+				main_model.curr_thetha = main_model.crossover(i);
 			}
 			aux_model.act_with_config_file();
-			aux_model.prepare_tmp_deep_ini_file(curr_thetha, main_model.optimizing_model_exe, main_model.param_opt_model);
+			aux_model.prepare_tmp_deep_ini_file(main_model.curr_thetha, main_model.optimizing_model_exe, main_model.param_opt_model);
 			error = aux_model.run();
-			alpha = main_model.get_statistics(curr_thetha, error, i);
+			alpha = main_model.get_statistics(main_model.curr_thetha, error, i);
 			if (alpha > 1)
 			{
-				new_posterior.thetha[i] = curr_thetha;
-				new_posterior.w[i] = main_model.generator.get_new_probabilities(new_posterior, curr_thetha, main_model.count_iter);//change-make generator private for abcde
+				main_model.new_posterior.thetha[i] = main_model.curr_thetha;
+				main_model.new_posterior.w[i] = main_model.generator.get_new_probabilities(main_model.new_posterior, main_model.curr_thetha, main_model.count_iter);//change-make generator private for abcde
 			}
 		}
 		print_log(t);
-		copy_posterior(main_model.posterior, new_posterior);
+		copy_posterior(main_model.posterior, main_model.new_posterior);
 	}
 }
 
