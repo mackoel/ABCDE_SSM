22adf5b6003eaf750dbc9f52184984c2689ae89d
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2020-04-12 14:57:45 +0300

    Update abcde.h

--- abcde.h
+++ abcde.h
@@ -10,7 +10,7 @@ public:
 	void act_with_config_file();
 	Distribution::Thetha mutation(int index);
 	Distribution::Thetha crossover(int index);
-	double get_statistics(Distribution::Thetha curr_thetha, double error, int i);
+	double get_statistics(Parametrs::MODE mode, Distribution::Thetha curr_thetha, double error, int i);
 	string config_file;
 	string deep_exe;
 	string optimizing_model_exe;
@@ -22,8 +22,7 @@ public:
 	double eps;
 	int t;
 	int count_iter;
+	int start_iter;
 	int count_thread;
 	double* error;
-	double delta;//for check
-
 };
