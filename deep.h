b31706c5f05620cbd1be7985562fc982a3306bd6
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2020-06-16 16:17:22 +0300

    Update deep.h

--- deep.h
+++ deep.h
@@ -6,8 +6,14 @@ class Deep:public Model
 {
 private:
 	pt::ptree propTree;
-
+	enum INI_MODE
+	{
+		FUNC_AND_CROPS = 0,
+		FUNC_WITH_CROPS
+	};
+	vector<string> name_opt_param = { "x;",  "beta;", "MB;",  "concs;", "phyl;", "PLACON;" , "PLAPOW30;" ,  "SLA;" , "TBRUE;" , "TP1RUE;" , "TP2RUE;" ,  "TCRUE;" ,  "KPAR;" ,  "IRUE1;" ,  "IRUE2;" ,  "FLF1A;" ,  "FLF1B;",  "WTOPL;",  "FLF2;", "FRTRL;",  "GCF;",  "PDHI;",  "WDHI1;", "WDHI2;",  "WDHI3;",  "WDHI4;",  "DEPORT;", "EED;",  "GRTDP;",  "TEC;",  "WSSG;",  "WSSL;",  "WSSD;",  "SLNG;",  "SLNS;",  "SNCG;", "SNCS;",  "GNC;",  "MXNUP;",  "WSSN;",  "TBD;",  "TP1D;", "TP2D;",  "TCD;",  "cpp;",  "ppsen;",  "ttSWEM;",  "ttEMR1;",  "ttR1R3;",  "ttR3R5;", "ttR5R7;",  "ttR7R8;",  "ttBRP;",  "ttTRP;",  "ttWSD;", "ttR1TLM;",  "ttR1TLP;",  "ttRUE;",  "ttBSG;",  "ttTSG;",  "ttBRG;",  "ttTRG;",  "ttBNF;",  "TRESH;", "ttDKill;",  "LtFtsw;",  "LtWdDur;",  "vpd_resp;",  "vpd_cr;" };
 public:
+	
 	Deep();
 
 	Deep(const string& param);
@@ -32,4 +38,6 @@ public:
 	int count_snp;
 	int count_weather_const;
 	int count_added_param;
+	int ini_mode;
+	int index_n, index_l;
 };
