68dbaae6e51c8d8aa5b54971ae534bffd427646f
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2020-07-12 14:10:54 +0300

    Update solution.h

--- solution.h
+++ solution.h
@@ -8,13 +8,20 @@ private:
 	Deep aux_model;
 	double error, alpha;
 	Parametrs param;
+	Run_manager manager;
 public:
 	Solution(const Abcde& _main_model, const Deep& _aux_model, const Parametrs& _param);
 
-	void run();
+	void run_init(int iter, int index_thetha);
+
+	void run_approximate(int iter, int index_thetha);
+
+	void run(int iter, int index_thetha);
 
 	void print_log(int iter);
 
 	void copy_posterior( Distribution::Posterior& posterior_to,  Distribution::Posterior& posterior_from);
+
+	void run_manager();
 
 };
