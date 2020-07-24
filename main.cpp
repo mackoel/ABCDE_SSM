b7b2564a02417f9b01b4c87e5b30afb3a6bc0d36
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2020-07-24 18:27:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "pch.h"
 
-int main(int argc, char *argv[])
+
+int main(int argc, char* argv[])
 {
+	int numTask, rank;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &numTask);
 	Parametrs param;
 	param.process_program_options(argc, argv);
 	Abcde abcde(param.config_file);
 	Deep deep(param.config_file);
 	Solution solution(abcde, deep, param);
 	solution.run_manager();
+	MPI_Finalize();
 	return 0;
 }
