9bcd66b028c241b1ee6c34b48a027da14b6c2eae
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2020-08-06 17:23:12 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,19 +1,6 @@
 #pragma once
 #include "pch.h"
 
-class Config
-{
-public:
-	int count_thread;
-	string name_ini_file;
-	string name_exe_file;
-	string name_command_exe_file;
-	string param_command_exe_file;
-	string curr_ini_file;
-	double eps;
-	int t;
-};
-
 class Parametrs
 {
 public:
