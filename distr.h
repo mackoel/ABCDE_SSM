60105287e4aea2aa4717fadce68f50576a2d4a18
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2020-08-17 12:03:28 +0300

    Update distr.h

--- distr.h
+++ distr.h
@@ -5,12 +5,12 @@ class Distribution
 {
     public:
 	    enum TYPE_DISTR
-        {
+            {
 	        NORM,
 	        EXPON,
 	        NORM_WITH_PARAM,
 	        RANDOM
-        };
+             };
 
 		typedef struct
 		{
@@ -26,28 +26,12 @@ class Distribution
 			double delta_one;
 		}Posterior;
 
-
 		double prior_distribution(TYPE_DISTR mode, const double param1 = 0.0, const double param2 = 0.0);
-
 		double getRandomSample(double param1, double param2);
-
-		double getNormalSample();
-		
+		double getNormalSample();	
 		double getNormalSampleWithParam(double mean, double var);
-
 		double getLrand(double l);
-		
-		double max_weight(double* w, const int size);
-		
-		Thetha get_prev_iter_with_weight(const Posterior& posterior, const int size);
-
-		double get_new_weight(Posterior& posterior, Thetha& curr_thetha, const int count_opt_param, const int count_iter, vector<double>& mean, vector<double>& std);
-
-		Thetha generate_vector_param(TYPE_DISTR mode, int count_opt_param, vector<double>& mean, vector<double>& std);
-
 		double kernelNormalSampleWithParam(double x, double mean, double var);
-		
 		double kernel_function(TYPE_DISTR mode, double x, const double param1 = 0.0, const double param2 = 0.0);
-
 		int generate_seed();
 };
