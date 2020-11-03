a1206bae44ecf6c69caf5ae2f4a980103bb82384
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2020-11-03 12:48:00 +0300

    Update abcde.h

--- abcde.h
+++ abcde.h
@@ -47,7 +47,8 @@ public:
 	double* error;
 	int count_opt_param;
 	vector<double> mean, std;
-	vector<double> sample_mean, sample_std;//sample param from popuatin
+	vector<double> sample_mean, sample_std;//sample param from population
+	vector<double> mut_dist_mean, mut_dist_std, cross_dist_mean, cross_dist_std;
 	double sample_error_mean;// sample error
 	vector<int> dtype;
 	vector<double> lbound, hbound;
